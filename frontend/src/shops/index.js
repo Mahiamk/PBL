57dabdac9f1fc641ee7c3d4d1f55a0b1a1096c23
Mahiamk <mahikomohammed@gmail.com>
2026-01-03 00:45:10 +0800

    Add API service for Axios configuration and create index file for shop components

---
+++ frontend/src/shops/index.js
@@ -0,0 +1,7 @@
+export { default as BarberShop } from './BarberShop/BarberShop';
+export { default as Tailor } from './Tailor/Tailor';
+export { default as ComputerShop } from './ComputerShop/ComputerShop';
+export { default as BottleShop } from './BottleShop/BottleShop';
+export { default as ClothingShop } from './ClothingShop/ClothingShop';
+export { default as DrinkShop } from './DrinkShop/DrinkShop';
+export { default as Massage } from './Massage/Massage';
