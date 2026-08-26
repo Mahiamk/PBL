6e36ca0cf324ea7ccd23996c47e71e5d594832dc
Mahiamk <mahikomohammed@gmail.com>
2026-08-27 02:45:06 +0800

    feat(frontend): configure application root and context providers in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { IconContext } from '@phosphor-icons/react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <IconContext.Provider
+      value={{
+        weight: 'duotone',
+        mirrored: false,
+      }}
+    >
+      <App />
+    </IconContext.Provider>
   </StrictMode>,
 )
